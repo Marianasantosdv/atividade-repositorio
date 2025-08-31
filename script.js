27448f2ab58de50105af09a1135dd631a012dcac
maripsicode <ttmarysociais@gmail.com>
2025-08-31 17:02:29 -0300

    my first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btnSurpresa");
+const mensagem = document.getElementById("mensagem");
+
+btn.addEventListener("click", () => {
+    mensagem.textContent = "Esse é o meu primeiro projeto no github!";
+});
